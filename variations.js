dce6381e02d33dc7e614bd4017a52f37824fe629
George Jalaghonia <giojala@Georges-iMac.local>
2020-03-20 02:03:01 +0400

    variations

---
+++ variations.js
@@ -0,0 +1,30 @@
+export function getNumberOfChars(name) {
+    return name.length;
+    
+}
+
+export function getFirstChar(name) {
+
+}
+
+export function getLastChar(name) {
+
+}
+
+export function getLower(name) {
+
+   
+
+}
+
+export function getUpper (name) {
+
+}
+
+export function getCapitalized(name) {
+
+}
+
+export function getClean(name) {
+
+}
